8bdaa96e2931cf2e5f6a3b60d3b1e2765b6a32b7
ifs21034-itdel <ifs21034@students.del.ac.id>
2024-03-21 14:00:46 +0700

    PR#10
    Perbaikan UTS

--- app/src/main/java/com/ifs21034/dinopedia/MainActivity.kt
+++ app/src/main/java/com/ifs21034/dinopedia/MainActivity.kt
@@ -8,11 +8,14 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val btnFamili = findViewById<Button>(R.id.btnFamili)
